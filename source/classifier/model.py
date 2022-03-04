14a8789d3c1bf7885c46e0d237dd4d2ea619a619
Nickil Maveli <nickilmaveli@gmail.com>
2022-03-04 01:52:39 +0000

    testing pytorch lightning

--- source/classifier/model.py
+++ source/classifier/model.py
@@ -1,14 +1,106 @@
+from typing import Optional
+
+import datasets
 import torch
+from torch.utils.data import DataLoader
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer, seed_everything
 from transformers import AdamW, AutoConfig, AutoModelForSequenceClassification, AutoTokenizer, get_linear_schedule_with_warmup
 
+from source.utils.prepare_dataset import PTBDataset
 
+class DataModule(LightningDataModule):
+
+    loader_columns = [
+        "datasets_idx",
+        "input_ids",
+        "token_type_ids",
+        "attention_mask",
+        "start_positions",
+        "end_positions",
+        "labels",
+    ]
+
+    def __init__(
+        self,
+        model_name_or_path: str,
+        data_path: str,
+        max_seq_length: int = 128,
+        train_batch_size: int = 32,
+        eval_batch_size: int = 32,
+        **kwargs,
+    ):
+        super().__init__()
+        self.model_name_or_path = model_name_or_path
+        self.data_path = data_path
+        self.max_seq_length = max_seq_length
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
+
+        self.text_fields = ["sentence"]
+        self.num_labels = 2
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path, use_fast=True)
+
+    def setup(self, stage: str):
+       
+        self.dataset = datasets.load_dataset('csv', data_files='sample.csv')
+        
+        print(self.dataset['train'])
+        print(self.dataset.keys())
+
+        for split in self.dataset.keys():
+            self.dataset[split] = self.dataset[split].map(
+                self.convert_to_features,
+                batched=True,
+                remove_columns=["label"],
+            )
+            self.columns = [c for c in self.dataset[split].column_names if c in self.loader_columns]
+            self.dataset[split].set_format(type="torch", columns=self.columns)
+
+        self.eval_splits = [x for x in self.dataset.keys() if "validation" in x]
+
+    def prepare_data(self):
+        datasets.load_dataset('csv', data_files='sample.csv')
+        AutoTokenizer.from_pretrained(self.model_name_or_path, use_fast=True)
+
+    def train_dataloader(self):
+        return DataLoader(self.dataset["train"], batch_size=self.train_batch_size)
+
+    def val_dataloader(self):
+        if len(self.eval_splits) == 1:
+            return DataLoader(self.dataset["validation"], batch_size=self.eval_batch_size)
+        elif len(self.eval_splits) > 1:
+            return [DataLoader(self.dataset[x], batch_size=self.eval_batch_size) for x in self.eval_splits]
+
+    def test_dataloader(self):
+        if len(self.eval_splits) == 1:
+            return DataLoader(self.dataset["test"], batch_size=self.eval_batch_size)
+        elif len(self.eval_splits) > 1:
+            return [DataLoader(self.dataset[x], batch_size=self.eval_batch_size) for x in self.eval_splits]
+
+    def convert_to_features(self, example_batch, indices=None):
+
+        # Either encode single sentence or sentence pairs
+        if len(self.text_fields) > 1:
+            texts_or_text_pairs = list(zip(example_batch[self.text_fields[0]], example_batch[self.text_fields[1]]))
+        else:
+            texts_or_text_pairs = example_batch[self.text_fields[0]]
+
+        # Tokenize the text/text pairs
+        features = self.tokenizer.batch_encode_plus(
+            texts_or_text_pairs, max_length=self.max_seq_length, pad_to_max_length=True, truncation=True
+        )
+
+        # Rename label to labels to make it easier to pass to model forward
+        features["labels"] = example_batch["label"]
+
+        return features
+    
+    
 class InsideOutsideStringClassifier(LightningModule):
     def __init__(
         self,
         model_name_or_path: str,
         num_labels: int,
-        task_name: str,
         learning_rate: float = 2e-5,
         adam_epsilon: float = 1e-8,
         warmup_steps: int = 0,
@@ -24,7 +116,6 @@ class InsideOutsideStringClassifier(LightningModule):
 
         self.config = AutoConfig.from_pretrained(model_name_or_path, num_labels=num_labels)
         self.model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path, config=self.config)
-        self.metric = datasets.load_metric("glue", self.hparams.task_name, experiment_id=datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))
 
     def forward(self, **inputs):
         return self.model(**inputs)
@@ -48,25 +139,10 @@ class InsideOutsideStringClassifier(LightningModule):
         return {"loss": val_loss, "preds": preds, "labels": labels}
 
     def validation_epoch_end(self, outputs):
-        if self.hparams.task_name == "mnli":
-            for i, output in enumerate(outputs):
-                # matched or mismatched
-                split = self.hparams.eval_splits[i].split("_")[-1]
-                preds = torch.cat([x["preds"] for x in output]).detach().cpu().numpy()
-                labels = torch.cat([x["labels"] for x in output]).detach().cpu().numpy()
-                loss = torch.stack([x["loss"] for x in output]).mean()
-                self.log(f"val_loss_{split}", loss, prog_bar=True)
-                split_metrics = {
-                    f"{k}_{split}": v for k, v in self.metric.compute(predictions=preds, references=labels).items()
-                }
-                self.log_dict(split_metrics, prog_bar=True)
-            return loss
-
         preds = torch.cat([x["preds"] for x in outputs]).detach().cpu().numpy()
         labels = torch.cat([x["labels"] for x in outputs]).detach().cpu().numpy()
         loss = torch.stack([x["loss"] for x in outputs]).mean()
         self.log("val_loss", loss, prog_bar=True)
-        self.log_dict(self.metric.compute(predictions=preds, references=labels), prog_bar=True)
         return loss
 
     def setup(self, stage=None) -> None:
@@ -106,15 +182,17 @@ class InsideOutsideStringClassifier(LightningModule):
 
     
 if __name__ == "__main__":
+    
+    ptb = PTBDataset(training_data_path="./data/PROCESSED/english/ptb-train-sentences-with-punctuation.txt")
+    
     seed_everything(42)
     AVAIL_GPUS = min(1, torch.cuda.device_count())
-    dm = DataModule(model_name_or_path="roberta-base")
+    dm = DataModule(model_name_or_path="roberta-base", data_path=ptb.aggregate_samples())
     dm.setup("fit")
     model = InsideOutsideStringClassifier(
-        model_name_or_path="albert-base-v2",
+        model_name_or_path="roberta-base",
         num_labels=dm.num_labels,
-        eval_splits=dm.eval_splits,
-        task_name=dm.task_name,
+        eval_splits=dm.eval_splits
     )
 
     trainer = Trainer(max_epochs=1, gpus=AVAIL_GPUS)
--- source/utils/prepare_dataset.py
+++ source/utils/prepare_dataset.py
@@ -22,11 +22,11 @@ class PTBDataset:
 
     def seed_bootstrap_distituent(self):
         distituent_slice_one = self.data['sentence'].str.split().str[:-1].str.join(" ")
-        distituent_slice_two = self.data[self.data['sentence'].str.split().str.len() > 30].str.split().str[:-2].str.join(" ")
-        distituent_slice_three = self.data[self.data['sentence'].str.split().str.len() > 40].str.split().str[:-3].str.join(" ")
-        distituent_slice_four = self.data[self.data['sentence'].str.split().str.len() > 50].str.split().str[:-4].str.join(" ")
-        distituent_slice_five = self.data[self.data['sentence'].str.split().str.len() > 60].str.split().str[:-5].str.join(" ")
-        distituent_slice_six = self.data[self.data['sentence'].str.split().str.len() > 70].str.split().str[:-6].str.join(" ")
+        distituent_slice_two = self.data[self.data['sentence'].str.split().str.len() > 30]['sentence'].str.split().str[:-2].str.join(" ")
+        distituent_slice_three = self.data[self.data['sentence'].str.split().str.len() > 40]['sentence'].str.split().str[:-3].str.join(" ")
+        distituent_slice_four = self.data[self.data['sentence'].str.split().str.len() > 50]['sentence'].str.split().str[:-4].str.join(" ")
+        distituent_slice_five = self.data[self.data['sentence'].str.split().str.len() > 60]['sentence'].str.split().str[:-5].str.join(" ")
+        distituent_slice_six = self.data[self.data['sentence'].str.split().str.len() > 70]['sentence'].str.split().str[:-6].str.join(" ")
         distituent_samples = pd.DataFrame(dict(sentence=pd.concat([distituent_slice_one, distituent_slice_two, 
                                                                    distituent_slice_three, distituent_slice_four,
                                                                    distituent_slice_five, distituent_slice_six]), 
@@ -34,10 +34,13 @@ class PTBDataset:
         return distituent_samples
 
     def aggregate_samples(self):
-        pass
+        constituent_samples = self.seed_bootstrap_constituent()
+        distituent_samples = self.seed_bootstrap_distituent()
+        df = pd.concat([constituent_samples, distituent_samples], ignore_index=True)
+        return pd.DataFrame(df.sample(n=200, random_state=42).reset_index(drop=True).to_dict())
 
     def train_validation_split(self):
-        
+        pass
 
 
 class CTBDataset:
@@ -56,4 +59,4 @@ class KTBDataset:
 if __name__ == "__main__":
     ptb = PTBDataset(training_data_path="./data/PROCESSED/english/ptb-train-sentences-with-punctuation.txt")
     # print(ptb.preprocess())
-    print(ptb.seed_bootstrap_constituent())
+    print(ptb.aggregate_samples().to_csv("sample.csv", index=False))
